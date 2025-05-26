cb69b60b1cca3ebe44f3167b685614f7b480516f
Abu_Sufiun <107828483+sufiun27@users.noreply.github.com>
2025-05-26 17:05:02 +0600

    git add

---
+++ main_branch.php
@@ -0,0 +1,9 @@
+<?php
+$main_branch = 'main branch';
+$value1 = $_POST['value1'];
+$value2 = $_POST['value2'];
+$value3 = $_POST['value3'];
+$value4 = $_POST['value4'];
+$value5 = $_POST['value5'];
+$value6 = $_POST['value6'];
+$value7 = $_POST['value7'];
