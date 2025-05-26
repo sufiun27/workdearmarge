7e60fb993bd38150318171d74db48acfadb68c8d
Abu_Sufiun <107828483+sufiun27@users.noreply.github.com>
2025-05-26 15:46:26 +0600

    dd3 file

---
+++ file1.php
@@ -0,0 +1,17 @@
+<?php
+$value1 = $_POST['value1'];
+$value2 = $_POST['value2'];
+$value3 = $_POST['value3'];
+$value4 = $_POST['value4'];
+$value5 = $_POST['value5'];
+$value6 = $_POST['value6'];
+$value7 = $_POST['value7'];
+$value8 = $_POST['value8'];
+$value9 = $_POST['value9'];
+$value10 = $_POST['value10'];
+$value11 = $_POST['value11'];
+$value12 = $_POST['value12'];
+$value13 = $_POST['value13'];
+$value14 = $_POST['value14'];
+$value15 = $_POST['value15'];
+$value16 = $_POST['value16'];
